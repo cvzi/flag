8f12bae131bd1fc6b9958e2aba24ad67b0f38cbe
cvzi <cuzi@openmail.cc>
2018-10-06 23:22:29 +0200

    Python3.7

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
